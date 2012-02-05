f5065c9febd4cf662901aad0e95df25e1e5fde65
Eric Palakovich Carr <carreric@gmail.com>
2012-02-05 08:26:50 -0500

    Removed test project for fandjango integration.  Fandjango integration tests were created in test moudle tests.

--- test_fandjango_integration/canvas/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    url(r'^test_url/$', 'canvas.views.test_url', name="test_url"),
-)
--- test_fandjango_integration/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls.defaults import patterns, include
-
-urlpatterns = patterns('',
-    (r'^canvas/', include('canvas.urls', namespace="canvas", app_name="canvas")),
-)
