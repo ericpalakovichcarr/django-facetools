02418e606784fe4947f1ad1dfa450648cf6b7006
Eric Palakovich Carr <epalakovichcarr@ecarr-imac.americanlegacy.org>
2012-02-08 14:38:56 -0500

    Updated cheeseshop meta data in setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,16 @@ from distutils.core import setup
 
 import facetools
 
+long_description = open("README.rst").read()
+
 setup(
     name = 'django-facetools',
     version = facetools.__version__,
     description = "Django Facetools provides a set of features to ease development of Facecbook canvas apps in a Django project.",
+    long_description = long_description,
     author = "Eric Palakovich Carr",
     author_email = "carreric@gmail.com",
+    license = "MIT",
     url = "https://github.com/bigsassy/django-facetools",
     packages = [
         'facetools',
@@ -19,5 +23,5 @@ setup(
         'facetools.migrations',
         'facetools.templatetags',
         'facetools.test',
-    ],
+    ]
 )
