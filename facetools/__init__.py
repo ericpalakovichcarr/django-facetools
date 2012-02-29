4aaa8e02aa1db08dae4ffdf8014ad72e7c0a39f2
Eric Palakovich Carr <epalakovichcarr@ecarr-imac.americanlegacy.org>
2012-02-29 15:21:23 -0500

    Upped version to 0.1.6

--- facetools/__init__.py
+++ facetools/__init__.py
@@ -3,5 +3,5 @@ try:
 except ImportError:
    from django.utils import simplejson as json
 
-__version__ = VERSION = '0.1.5'
+__version__ = VERSION = '0.1.6'
 __version_info__ = tuple(__version__.split('.'))
