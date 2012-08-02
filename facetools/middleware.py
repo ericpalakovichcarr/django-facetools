7cd9e49aefa1a0c296fa4440a2f2e2a03700c839
Eric Palakovich Carr <carreric@gmail.com>
2012-08-01 23:16:02 -0400

    Removed FacebookRedirectMiddleware

--- facetools/middleware.py
+++ facetools/middleware.py
@@ -8,30 +8,6 @@ from facetools.url import translate_url_to_facebook_url, facebook_redirect
 
 GET_REDIRECT_PARAM = 'facebook_redirect'
 
-class FacebookRedirectMiddleware(object):
-    def process_request(self, request):
-        # Look for GET parameter, and redirect if you find it
-        redirect_url = request.GET.get(GET_REDIRECT_PARAM)
-        if redirect_url:
-            return facebook_redirect(redirect_url)
-        return None
-
-    def process_response(self, request, response):
-        if type(response) == HttpResponseRedirect:
-            facebook_url = translate_url_to_facebook_url(response['Location'])
-            if facebook_url != response['Location']:
-                get_params = urllib.urlencode({GET_REDIRECT_PARAM: facebook_url})
-                prefix = "?"
-                insert_position = len(response['Location'])
-                if '?' in response['Location']:
-                    prefix = "&"
-                if "#" in response['Location']:
-                    insert_position = response['Location'].index("#")
-                response['Location'] = response['Location'][:insert_position] + \
-                                       prefix + get_params + \
-                                       response['Location'][insert_position:]
-        return response
-
 class FandjangoIntegrationMiddleware(object):
 
     def process_response(self, request, response):
--- test_project/facetools_tests/tests/test_middleware.py
+++ test_project/facetools_tests/tests/test_middleware.py
@@ -1,74 +1,10 @@
-import os
 import urlparse
-import re
 
 from django.test import TestCase
-from django.conf import settings
-from django.http import HttpResponseRedirect, HttpRequest
-
-from facetools.middleware import FacebookRedirectMiddleware, FandjangoIntegrationMiddleware, GET_REDIRECT_PARAM
-from test_project.canvas.views import test_url
 from fandjango.views import authorize_application
-import fandjango
 
-class FacebookRedirectMiddlewareTests(TestCase):
-
-    def test_response_and_request(self):
-        expected_results = [
-            ("/canvas/view", "/view"),
-            ("/canvas/view#fragment", "/view#fragment"),
-            ("/canvas/view/", "/view/"),
-            ("/canvas/view/#fragment", "/view/#fragment"),
-            ("/canvas/view/?hi=hey", "/view/?hi=hey"),
-            ("/canvas/view/?hi=hey#fragment", "/view/?hi=hey#fragment"),
-            ("/canvas/view/?hi=hey&pow=1", "/view/?hi=hey&pow=1"),
-            ("/canvas/view/?hi=hey&pow=1#fragment", "/view/?hi=hey&pow=1#fragment"),
-            ("/canvas/view?hi=hey", "/view?hi=hey"),
-            ("/canvas/view?hi=hey#fragment", "/view?hi=hey#fragment"),
-            ("/canvas/view?hi=hey&pow=1", "/view?hi=hey&pow=1"),
-            ("/canvas/view?hi=hey&pow=1#fragment", "/view?hi=hey&pow=1#fragment"),
-            ("http://google.com", None),
-            ("http://www.google.com", None),
-            ("https://www.google.com", None),
-            ("https://www.google.com/whateves", None),
-            ("https://www.google.com/whateves/", None),
-            ("https://google.com/whateves", None),
-            ("https://google.com/whateves/", None),
-            ("www.google.com", None),
-            ("www.google.com/hey", None),
-            ("google.com/yo", None),
-            ("google.com/loud/noises/", None),
-            ("/not_canvas/view", None),
-            ("/not_canvas/view#fragment", None),
-            ("/not_canvas/view/", None),
-            ("/not_canvas/view/#fragment", None),
-            ("/not_canvas/view/?hi=hey", None),
-            ("/not_canvas/view/?hi=hey#fragment", None),
-            ("/not_canvas/view/?hi=hey&pow=1", None),
-            ("/not_canvas/view/?hi=hey&pow=1#fragment", None),
-            ("/not_canvas/view?hi=hey", None),
-            ("/not_canvas/view?hi=hey#fragment", None),
-            ("/not_canvas/view?hi=hey&pow=1", None),
-            ("/not_canvas/view?hi=hey&pow=1#fragment", None),
-        ]
-
-        middleware = FacebookRedirectMiddleware()
-        for original_redirect,new_redirect in expected_results:
-            mock_response = HttpResponseRedirect(original_redirect)
-            new_response = middleware.process_response(None, mock_response)
-            if new_redirect is None:
-                self.assertEquals(original_redirect, new_response['Location'])
-            else:
-                new_redirect = settings.FACEBOOK_CANVAS_PAGE + new_redirect
-                self.assertTrue("?" in new_response['Location'])
-                query = urlparse.parse_qs(urlparse.urlparse(new_response['Location']).query)
-                self.assertEquals(new_redirect, query[GET_REDIRECT_PARAM][0])
-
-                # Now test the request handler
-                mock_request = HttpRequest()
-                mock_request.GET[GET_REDIRECT_PARAM] = query[GET_REDIRECT_PARAM][0]
-                response = middleware.process_request(mock_request)
-                self.assertIn('top.location.href="%s"' % query[GET_REDIRECT_PARAM][0], response.content)
+from facetools.middleware import FandjangoIntegrationMiddleware, GET_REDIRECT_PARAM
+from test_project.canvas.views import test_url
 
 class FandjangoMiddlewareTests(TestCase):
 
